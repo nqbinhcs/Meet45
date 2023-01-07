22d0d60322987ce532a75735b37d85b3b1edc9cc
nqbinhcs <quangbinh047@gmail.com>
2023-01-07 11:54:55 +0700

    Add docker compose file

--- src/server/server.js
+++ src/server/server.js
@@ -16,7 +16,11 @@ app.use(express.urlencoded({ extended: false }));
 
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: [
+      "http://localhost:3000",
+      "http://localhost:3001",
+      "http://localhost:3002",
+    ],
   })
 );
 
